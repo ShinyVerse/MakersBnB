caabf92e31df784f2e15e54e1a4df162c7ba4d09
Andres Faraone <andresfaraone@outlook.com>
2018-10-09 18:51:19 +0100

    Users now connects to db and returns json in API for /

--- server/users.js
+++ server/users.js
@@ -1,16 +1,31 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var router = express.Router();
-var users = ['John', 'Betty', 'Hal'];
+var users = []
+
+//require user to access database table
+var User = require('./database_user')
 
 
 
-
-
+// displays all entries in users
 router.get('/', function (req, res) {
-  res.json(users);
+  User.find((err, users) => {
+    if (err) return res.status(500).send(err)
+    return res.json(users);
+  })
 });
 
+//temporary route to populate the database
+router.get('/populate', function(req, res) {
+  var dog = new User({ name: 'dog', email: "dog@dog.com", password: 'dog123' });
+  var cat = new User({ name: 'cat', email: "cat@dog.com", password: 'cat123' });
+  dog.save();
+  cat.save();
+  res.redirect('/users')
+})
+
+// create a new entry in db
 router.post('/', function (req, res) {
   users.push(req.body.name)
   res.status(201).send()
@@ -18,10 +33,12 @@ router.post('/', function (req, res) {
   res.redirect('/users')
 });
 
+// displays individual entry
 router.get('/:id', function (req, res) {
   res.json(users[req.params.id]);
 });
 
+// updates individual entry
 router.put('/:id', function(req, res) {
   if (users.length > req.params.id) {
     users[req.params.id] = req.body.name;
@@ -32,6 +49,7 @@ router.put('/:id', function(req, res) {
   res.redirect('/users')
 });
 
+// deletes an individual entry
 router.delete('/:id', function(req, res) {
   if (users.length > req.params.id) {
     users.splice(req.params.id, 1);
