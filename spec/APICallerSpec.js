78ebef3f8ef4e400dfc2630477b113df180c5de3
Cameron Whitehead <camjw119@gmail.com>
2018-10-09 16:26:38 +0100

    Got APICaller working with jQuery dependency injected

--- spec/APICallerSpec.js
+++ spec/APICallerSpec.js
@@ -2,23 +2,39 @@ var APICaller = require("../src/APICaller.js");
 
 describe("APICaller", function(){
   var subject;
+  const testURL = 'https://jsonplaceholder.typicode.com/todos/'
 
   beforeEach(function(){
-    mockjQuery = jasmine.createSpyObj('mockjQuery', { 'post': 202 })
-    subject = new APICaller(mockjQuery);
+    mockjQuery = jasmine.createSpyObj('mockjQuery', { 'post': 202, 'get': 200 })
+    mockBcrypt = jasmine.createSpyObj('mockBcrypt', {'genSaltSync': 10, 'hashSync': 'fewiofjweio'})
+    subject = new APICaller(testURL, mockjQuery, mockBcrypt);
+  });
+
+  describe('#hashPassword', function(){
+    it('returns hash with encrypted password', function(){
+      subject.hashPassword('fewiofjweio')
+      expect(mockBcrypt.genSaltSync).toHaveBeenCalledWith(10);
+      expect(mockBcrypt.hashSync).toHaveBeenCalledWith("password", 10);
+    });
   });
 
   describe("#sendNewUser", function(){
     it("sends a new user to the API", function(){
       subject.sendNewUser('Billy', 'billy@mail.co', 'fewiofjweio')
       expected_request = {
-        type: "POST",
-        url: 'http://cd2ed5b4.ngrok.io/users',
+        url: testURL,
         name: 'Billy',
         email: 'billy@mail.co',
         password: 'fewiofjweio'
       }
       expect(mockjQuery.post).toHaveBeenCalledWith(expected_request)
+    });
+  });
+
+  describe('#getUserFromDatabase', function(){
+    it("returns a user from the database", function(){
+      subject.getUserFromDatabase(0)
+      expect(mockjQuery.get).toHaveBeenCalledWith(ID)
     });
   });
 })
--- src/APICaller.js
+++ src/APICaller.js
@@ -1,17 +1,29 @@
-function APICaller(jquery) {
-  this.jquery = jquery
+function APICaller(rootURL, jquery, bcrypt) {
+  this.rootURL = rootURL
+  this.jquery = jquery;
+  this.bcrypt = bcrypt;
 };
 
 APICaller.prototype.sendNewUser = function(name, email, password) {
   this.jquery.post({
-    type: "POST",
-    url: 'http://cd2ed5b4.ngrok.io/users',
+    url: this.rootURL,
     name: name,
     email: email,
-    password: password
+    password: this.hashPassword(password)
   });
 }
 
+APICaller.prototype.hashPassword = function (password){
+  const salt = this.bcrypt.genSaltSync(10);
+  return this.bcrypt.hashSync("password", salt);
+}
+
+APICaller.prototype.getUserFromDatabase = function(id) {
+   this.jquery.get(this.rootURL + id, function(data) {
+     console.log(data)
+     return data;
+   })
+};
 
 // Export node module.
 if (typeof module !== 'undefined' && module.hasOwnProperty('exports')) {
