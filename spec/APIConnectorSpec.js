2745dcc522bcc6a89b6914f773ada321f117e486
Laura Jackson <playeronereadystart@gmail.com>
2018-10-10 17:22:12 +0100

    Add create and post.

--- spec/APIConnectorSpec.js
+++ spec/APIConnectorSpec.js
@@ -8,8 +8,6 @@ describe("APIConnector", function() {
       [ { email: 'betty@mail.co.uk', password: 'hfowepfmoamopaivgnpeanpv'} ] });
     mockRoot = "www.mockapi.com"
     subject = new APIConnector(mockjQuery, mockRoot);
-    console.log(subject.jquery.get())
-    console.log(subject.rootURL)
   })
 
 
@@ -19,6 +17,20 @@ describe("APIConnector", function() {
       subject.connect('get', '/users/0').then(function(res) {
         expect(res).toEqual([{email: 'betty@mail.co.uk', password: 'hfowepfmoamopaivgnpeanpv'}])
       })
+    })
+
+    it('calls jquery post if verb is post', function() {
+      var send_param = { name: 'Billy', email: 'billy@mail.co.uk', password: 'fewiofjweio'}
+      subject.connect('post', '/users', send_param)
+
+      var expected_request = {
+        url: "www.mockapi.com/users",
+        name: 'Billy',
+        email: 'billy@mail.co.uk',
+        password: 'fewiofjweio'
+      }
+
+      expect(mockjQuery.post).toHaveBeenCalledWith(expected_request)
     })
   })
 })
