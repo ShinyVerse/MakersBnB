657d5ec37dffa3615647b9ea5c5c96368b6e40bd
Andres Faraone <andresfaraone@outlook.com>
2018-10-11 12:28:15 +0100

    finished api

--- server/index.js
+++ server/index.js
@@ -1,27 +1,22 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-// var db_model = require('./database_user')
 var app = express();
+var bodyParser = require('body-parser');
 
-//connects to database and posts confirmation on console
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', function() {
-  console.log('connected!');
-})
+app.use(bodyParser.json())
 
 // defines each router
 var usersRouter = require('./users.js');
-var listingsRouter = require('./listings.js');
-var bookingsRouter = require('./bookings.js');
-
-// tells body parser to use JSON encoding?
-app.use(bodyParser.json())
-
-// sets the urls for each routers
 app.use('/users', usersRouter);
-app.use('/listings', listingsRouter);
-app.use('/bookings', bookingsRouter);
+
+if (process.env.NODE_ENV !== 'test') {
+  var listingsRouter = require('./listings.js');
+  var bookingsRouter = require('./bookings.js');
+
+  // sets the urls for each routers
+  app.use('/listings', listingsRouter);
+  app.use('/bookings', bookingsRouter);
+}
+
+// tells body parser to use JSON encoding
 
 module.exports = app;
