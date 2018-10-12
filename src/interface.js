e1f2467a06d8bca861bf5e2cef380a2a954fffbd
Laura Jackson <playeronereadystart@gmail.com>
2018-10-12 11:49:25 +0100

    Add listings div generator

--- src/interface.js
+++ src/interface.js
@@ -34,7 +34,20 @@ $(document).ready(function() {
   })
 
   listHand.queryListings().then(function(res) {
-    $('#allListings').text(list)
+
+    for (var i = 0; i < res.length; i += 1){
+      let id = res[i]._id;
+      let bookingId = "book" + id;
+      let address = "<li> "+ res[i].address + "</li>"
+      let beds = "<li> "+ res[i].no_beds + "</li>"
+      let bookButton = "<button id=" + bookingId + " class=btn>Book now!</button>"
+      $('#allListings').append("<div class=listItem id=" + id + ">"
+      + address
+      + beds
+      + bookButton
+      +"</div>");
+    }
+    // $('#allListings').text(list)
   } )
 
 
